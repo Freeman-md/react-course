4f4ad6251469555011ef24926061c84cfb6ac271
Freeman-md <freemansparda@gmail.com>
2021-06-29 23:02:47 +0100

    Conditional Rendering - [Ternary Operators, Inline If's and Logical And Operator]

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,8 @@ console.log('App.js is running');
 
 var app = {
   title: 'Indecision App',
-  subtitle: 'This is some info'
+  subtitle: 'This is some info',
+  options: ['One', 'Two']
 
   // JSX - JavaScript XML (Expression)
 };var template = React.createElement(
@@ -15,10 +16,15 @@ var app = {
     null,
     app.title
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     'p',
     null,
     app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
   ),
   React.createElement(
     'ol',
@@ -41,28 +47,35 @@ var user = {
   age: 26,
   location: 'Philadelphia'
 };
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
 var templateTwo = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    user.name
+    user.name ? user.name : 'Andeyfocus'
   ),
-  React.createElement(
+  user.age && user.age >= 18 && React.createElement(
     'p',
     null,
     'Age: ',
     user.age
   ),
-  React.createElement(
-    'p',
-    null,
-    'Location: ',
-    user.location
-  )
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
