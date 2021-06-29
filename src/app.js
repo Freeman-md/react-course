8a68de5e5ba3d3b502dfd94006f3ab4f2686f2da
Freeman-md <freemansparda@gmail.com>
2021-06-29 22:17:45 +0100

    Exploring JSX - Nested Elements & Formatting

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 console.log('App.js is running');
 
-// JSX - JavaScript XML
-var template = <p>Indecision App</p>;
+// JSX - JavaScript XML (Expression)
+var template = (
+  <div>
+    <h1>Indecision App</h1> 
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+    <h1>Andrew Mead</h1>
+    <p>Age: 26</p>
+    <p>Location: Philadelphia</p>
+  </div>
+)
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot); 
+ReactDOM.render(templateTwo, appRoot);
