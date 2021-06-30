84695a998fd5a5bedde4bc305721d771db9f1c77
Freeman-md <freemansparda@gmail.com>
2021-06-30 05:51:58 +0100

    Events and Attributes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -42,40 +42,42 @@ var app = {
   )
 );
 
-var user = {
-  name: 'Andrew',
-  age: 26,
-  location: 'Philadelphia'
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
 };
-
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      location
-    );
-  }
-}
-
+var minusOne = function minusOne() {
+  console.log('minusOne');
+};
+var reset = function reset() {
+  console.log('reset');
+};
 var templateTwo = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    user.name ? user.name : 'Andeyfocus'
+    'Counter: ',
+    count
   ),
-  user.age && user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
   ),
-  getLocation(user.location)
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'Reset'
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
